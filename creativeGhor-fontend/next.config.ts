d375d966f732c1334162fdff274d0fc958e858d1
Tanvir Ahamed <ahamedtanvir374@gmail.com>
2025-02-19 22:55:18 +0600

    vercel deploy

--- creativeGhor-fontend/next.config.ts
+++ creativeGhor-fontend/next.config.ts
@@ -1,7 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   images: {
     domains: ["res.cloudinary.com"], // Add Cloudinary to allowed domains
   },
